e049001b73a55b31448ae90dded06a6a2e21bc52
Panquestia <72416685+wow-rambler-project@users.noreply.github.com>
2021-03-07 01:18:23 +0100

    Added WoWRamblerProjectQuestSnapshots

--- main.lua
+++ main.lua
@@ -10,30 +10,6 @@ mainFrame.events = {}
 
 local Version, Build = GetBuildInfo()
 
-local function ArrayUniqueDifference(minuend, subtrahend)
-	local tempArray = {}
-
-	for k, v in pairs(minuend) do
-		tempArray[v] = true
-	end
-
-	for k, v in pairs(subtrahend) do
-		tempArray[v] = false
-	end
-
-	local difference = {}
-	local n = 0
-
-	for k, v in pairs(tempArray) do
-		if v then
-			n = n + 1
-			difference[n] = k
-		end
-	end
-
-	return difference, n
-end
-
 function mainFrame:SetupEvents()
 	self:SetScript("OnEvent", function(self, event, ...)
 		self.events[event](self, ...)
@@ -49,6 +25,7 @@ function mainFrame.events:ADDON_LOADED(addonName)
 		WoWRamblerProjectQuestLog = WoWRamblerProjectQuestLog or {}
 		WoWRamblerProjectHiddenQuests = WoWRamblerProjectHiddenQuests or {}
 		WoWRamblerProjectQuestsDone = WoWRamblerProjectQuestsDone or {}
+		WoWRamblerProjectQuestSnapshots = WoWRamblerProjectQuestSnapshots or {}
 	end
 end
 
@@ -67,19 +44,19 @@ function mainFrame:AddLogEntry(event, questId)
 
 	WoWRamblerProjectQuestLog[questId] = WoWRamblerProjectQuestLog[questId] or {}
 	WoWRamblerProjectQuestLog[questId][GetServerTime()] = {
-		["event"] = event or "nil",
-		["title"] = C_QuestLog.GetTitleForQuestID(questId) or "nil",
+		["event"] = event or nil,
+		["title"] = C_QuestLog.GetTitleForQuestID(questId) or nil,
 		["datetime"] = string.format("%s @ %s (%s)", date(), Version, Build),
-		["instance id"] = instanceId or "nil",
-		["instance x"] = x or "nil",
-		["instance y"] = y or "nil",	
-		["map id"] = uiMapID or "nil",
-		["map x"] = mapX or "nil",
-		["map y"] = mapY or "nil",
-		["zone"] = GetRealZoneText() or "nil",
-		["subzone"] = GetSubZoneText() or "nil",
-		["target"] = UnitName("target") or "nil",
-		["target GUID"] = UnitGUID("target") or "nil"
+		["instance id"] = instanceId or nil,
+		["instance x"] = x or nil,
+		["instance y"] = y or nil,	
+		["map id"] = uiMapID or nil,
+		["map x"] = mapX or nil,
+		["map y"] = mapY or nil,
+		["zone"] = GetRealZoneText() or nil,
+		["subzone"] = GetSubZoneText() or nil,
+		["target"] = UnitName("target") or nil,
+		["target GUID"] = UnitGUID("target") or nil
 	}
 end
 
@@ -88,24 +65,8 @@ function mainFrame.events:QUEST_ACCEPTED(questId)
 end
 
 function mainFrame.events:QUEST_TURNED_IN(questId)
-	local serverQuests = C_QuestLog.GetAllCompletedQuestIDs()
-	
-	-- Make sure serverQuests contains the just completed quest.
-	table.insert(serverQuests, questId)
-
-	local diff, count = ArrayUniqueDifference(serverQuests, WoWRamblerProjectQuestsDone)
-	WoWRamblerProjectQuestsDone = serverQuests
-
-	if count > 1 then
-		-- Okay, there is at least one additional (hidden) quest completed.
-		WoWRamblerProjectHiddenQuests[questId] = {}
-	
-		for k, v in pairs(diff) do
-			if questId ~= v then
-				table.insert(WoWRamblerProjectHiddenQuests[questId], v)
-			end
-		end
-	end
+	WoWRamblerProjectQuestSnapshots[questId] = WoWRamblerProjectQuestSnapshots[questId] or {}
+	WoWRamblerProjectQuestSnapshots[questId][GetServerTime()] = C_QuestLog.GetAllCompletedQuestIDs()
 
 	self:AddLogEntry("QUEST_TURNED_IN", questId)
 end
